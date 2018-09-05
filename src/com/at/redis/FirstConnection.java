b0ea7c065274844f5a636dfc4b99ba9f31c17b60
ouakira <1638897848@qq.com>
2018-09-05 17:40:11 +0800

    redis distributed key

--- src/com/at/redis/FirstConnection.java
+++ src/com/at/redis/FirstConnection.java
@@ -5,20 +5,11 @@ import redis.clients.jedis.Jedis;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.UUID;
 
 public class FirstConnection {
     public static void main(String[] args){
-        //连接本地的 Redis 服务
-        Jedis jedis = new Jedis("47.94.242.46");
-        System.out.println("连接成功");
-        //查看服务是否运行
-        System.out.println("服务正在运行: "+jedis.ping());
-        Set<String> redisKeys=jedis.keys("*");
-        Iterator iterator=redisKeys.iterator();
-        while (iterator.hasNext()){
-            String s=(String) iterator.next();
-            System.out.println("redis key---"+s+"value----"+jedis.get(s));
-        }
-        //System.out.println(null instanceof String);
+        Jedis jedis=RedisTool.getConnection("127.0.0.1","wangxu960722");
+        RedisTool.tryGetDistributedLock(jedis,"123", UUID.randomUUID().toString(),100000);
     }
 }
