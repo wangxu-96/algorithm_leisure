14a13988d437d7383d4ea8f8aa4e8dc556a98251
wangx <1638897848@qq.com>
2020-07-29 11:13:10 +0800

    update code

---
+++ src/com/at/wangxu/leetcode/LevelOrder.java
@@ -0,0 +1,86 @@
+package com.at.wangxu.leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 102. 二叉树的层序遍历
+ * <p>
+ * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * 二叉树：[3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回其层次遍历结果：
+ * <p>
+ * [
+ * [3],
+ * [9,20],
+ * [15,7]
+ * ]
+ */
+public class LevelOrder {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<>();
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            //每次取出队列中的全部数据，即每次遍历树中的一层
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                assert poll != null;
+                list.add(poll.val);
+                if (poll.left != null){
+                    queue.add(poll.left);
+                }
+                if (poll.right != null){
+                    queue.add(poll.right);
+                }
+            }
+            lists.add(list);
+        }
+        return lists;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        List<List<Integer>> lists = levelOrder(root);
+        for (List<Integer> list : lists) {
+            for (Integer integer : list) {
+                System.out.print(integer);
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
