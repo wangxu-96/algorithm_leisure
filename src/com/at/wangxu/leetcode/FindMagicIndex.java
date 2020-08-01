f91d01dd100368e838214a6b574b4fc2cc6e56f4
wangx <1638897848@qq.com>
2020-08-01 08:02:45 +0800

    面试题 08.03. 魔术索引

--- src/com/at/wangxu/leetcode/FindMagicIndex.java
+++ src/com/at/wangxu/leetcode/FindMagicIndex.java
@@ -29,7 +29,7 @@ public class FindMagicIndex {
 //                return i;
 //        }
 //        return -1;
-        return findMin(nums, 0, nums.length);
+        return findMin(nums, 0, nums.length-1);
     }
 
     public int findMin(int[] nums,int l,int r) {
@@ -41,8 +41,8 @@ public class FindMagicIndex {
         int min = findMin(nums, l, m - 1);
         if (min != -1)
             return min;
-        //判断当前节点
-        if (nums[m] == m)
+            //判断当前节点
+        else if (nums[m] == m)
             return m;
         //再找右边
         return findMin(nums, m + 1, r);
@@ -51,6 +51,6 @@ public class FindMagicIndex {
     public static void main(String[] args) {
         FindMagicIndex magicIndex = new FindMagicIndex();
         System.out.println(magicIndex.findMagicIndex(new int[]{0, 2, 3, 4, 5}));
-        System.out.println(magicIndex.findMagicIndex(new int[]{1, 1, 1}));
+        System.out.println(magicIndex.findMagicIndex(new int[]{12296169, 14481887, 19365401, 71948694, 101256536, 137449705, 147615033, 169095970, 182939974, 183054331, 191033174, 200069688, 210281043, 211549396, 227193353, 252408327, 263757832, 268669870, 271916258, 293898012, 322628245, 329246885, 348479255, 405807814, 431800160, 449369511, 477566467, 481431749, 481880069, 487953610, 509211052, 520721303, 527744664, 550058864, 571603718, 571617555, 579098239, 582152388, 645340207, 681566032, 685774515, 706348591, 708774328, 717815831, 721421995, 724666698, 745560058, 746289154, 769651867, 781893631, 789714924, 807615645, 882508445, 884260053, 916797901, 920985226, 924045345, 932899253, 950715182, 987825772, 1015158842, 1016121780, 1065377233, 1072449577}));
     }
 }
