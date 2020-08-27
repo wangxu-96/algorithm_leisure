7df5303df8119311073d55a9b7d6c160e26e7006
leisure <leisure960722@gmail.com>
2020-08-27 11:36:32 +0800

    491. 递增子序列 17. 电话号码的字母组合 201. 数字范围按位与 459. 重复的子字符串

--- src/com/at/wangxu/String/InternTest.java
+++ src/com/at/wangxu/String/InternTest.java
@@ -5,7 +5,11 @@ public class InternTest {
     public static void main(String[] args) {
         String s1 = "abc";
         String s2 = new String("abc");
-        s2.intern();
-        System.out.println(s1 == s2);
+        String s3 = new String("abc");
+        String s4 = "ab" + "c";
+        String s5 = "ab";
+        String s6 = "c";
+        String s7 = s5 + s6;
+        System.out.println(s1 == s4);
     }
 }
--- src/com/at/wangxu/Thread/LockDemo.java
+++ src/com/at/wangxu/Thread/LockDemo.java
@@ -1,24 +1,48 @@
 package com.at.wangxu.Thread;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class LockDemo {
     private static final ReentrantLock lock=new ReentrantLock();
     public static void main(String[] args){
-        for (int i = 0; i < 5; i++) {
-            Thread thread=new Thread(()->{
+
+        Thread thread = new Thread(()->{
+            try {
                 lock.lock();
-                try {
-//                    lock.lock();
-                    System.out.println(Thread.currentThread().getName() + " get lock");
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }finally {
-                    lock.unlock();
-                }
-            });
-            thread.start();
-        }
+                Thread.sleep(3000l);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+            System.out.println("thread 0 exit");
+        });
+
+        Thread thread2 = new Thread(()->{
+            try {
+                lock.lock();
+            }  finally {
+                lock.unlock();
+            }
+            System.out.println("thread 2 exit");
+        });
+
+        Thread thread1 = new Thread(()->{
+            try {
+                lock.tryLock(1, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+            System.out.println("thread 1 exit");
+        });
+
+        thread.start();
+        thread2.start();
+        thread1.start();
+
+        thread1.interrupt();
     }
 }
